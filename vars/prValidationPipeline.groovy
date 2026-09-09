5b93e92e634fb637374239cdf4105fa58b225437
abhishek-kurahatti <abhishek.kurahatti@tarento.com>
2026-09-09 18:21:32 +0530

    Change SonarQube working directory path

--- vars/prValidationPipeline.groovy
+++ vars/prValidationPipeline.groovy
@@ -141,7 +141,7 @@ pipeline {
                                   -Dsonar.pullrequest.branch="${env.CHANGE_BRANCH}" \
                                   -Dsonar.pullrequest.base="${env.CHANGE_TARGET}" \
                                   -Dsonar.exclusions="**/.venv/**,**/venv/**,**/__pycache__/**,**/*.pyc" \
-                                  -Dsonar.working.directory=/usr/src/.scannerwork
+                                  -Dsonar.working.directory=.scannerwork
                             """
 
                         }
