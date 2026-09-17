c612edf1a65197112151428342d14dfef844debf
abhishek-kurahatti <abhishek.kurahatti@tarento.com>
2026-09-17 17:44:25 +0530

    Fix indentation and formatting in prValidationPipeline

--- vars/prValidationPipeline.groovy
+++ vars/prValidationPipeline.groovy
@@ -143,6 +143,8 @@ pipeline {
                               java -version
                                 ${scannerHome}/bin/sonar-scanner \
                                   -Dsonar.scanner.skipJreProvisioning=true \
+                                  -Dsonar.host.url="${SONAR_HOST_URL}"
+                                  -Dsonar.token="${SONAR_AUTH_TOKEN}"
                                   -Dsonar.projectKey="${repoName}" \
                                   -Dsonar.sources=src \
                                   -Dsonar.tests=src \
