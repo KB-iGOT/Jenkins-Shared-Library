b0c047fdc027d5b6d82e7cb7a78828f4aa492e4c
abhishek-kurahatti <abhishek.kurahatti@tarento.com>
2026-09-17 11:01:02 +0530

    Update SonarQube source and test paths

--- vars/prValidationPipeline.groovy
+++ vars/prValidationPipeline.groovy
@@ -140,11 +140,11 @@ pipeline {
                                   -Dsonar.userHome=/opt/sonar-cache \
                                   -Dsonar.verbose=true \
                                   -Dsonar.projectKey="${repoName}" \
-                                  -Dsonar.sources=src/app \
+                                  -Dsonar.sources=src \
                                   -Dsonar.pullrequest.key="${env.CHANGE_ID}" \
                                   -Dsonar.pullrequest.branch="${env.CHANGE_BRANCH}" \
                                   -Dsonar.pullrequest.base="${env.CHANGE_TARGET}" \
-                                  -Dsonar.tests=src/app \
+                                  -Dsonar.tests=src \
                                   -Dsonar.test.inclusions="**/*.spec.ts" \
                                   -Dsonar.typescript.lcov.reportPaths=coverage/lcov.info \
                                   -Dsonar.exclusions="**/node_modules/**,**/*.module.ts,**/*.model.ts,**/*.interface.ts,**/*.enum.ts,**/*.routing.ts,**/*.routes.ts,**/*.spec.ts,**/*.mock.ts,**/*.stub.ts,**/*setup-jest.ts,**/*main.ts,**/*environment.*.ts,**/*test.ts,**/assets/**,**/mdo-assets/**,**/themes/**,**/styles/**,**/coverage/**,**/dist/**,**/.angular/**,protractor.conf.js,babel.config.js,jest.config.js,jest.env.js,test/mocks/*.*,karma.conf.js"
